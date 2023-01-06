8c81eadb73491284b4333ddc2d6922a9814e90cb
Pugpaprika21 <pugpaprika21@gmail.com>
2023-01-06 04:48:25 -0800

    e

--- main.go
+++ main.go
@@ -13,10 +13,7 @@ type NewsAggPage struct {
 
 func newAggHandler(w http.ResponseWriter, r *http.Request) {
 	p := NewsAggPage{Title: "Hahaha", News: "Some News"}
-	t, err := template.ParseFiles("template/basictemplating.html")
-	if err != nil {
-		return
-	}
+	t, _ := template.ParseFiles("template/basictemplating.html")
 	t.Execute(w, p)
 }
 
@@ -24,8 +21,22 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello GO</h1>")
 }
 
+func loginPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		p := NewsAggPage{Title: r.Method, News: "Login Error"}
+		t, _ := template.ParseFiles("template/loginErr.html")
+		t.Execute(w, p)
+	} else {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)      // http://localhost:8080/
 	http.HandleFunc("/agg/", newAggHandler) // http://localhost:8080/agg/
+	http.HandleFunc("/login/", loginPage)   // http://localhost:8080/login/
 	http.ListenAndServe(":8080", nil)
 }
