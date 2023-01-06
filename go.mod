e9ba5760bf3f46c0f19213018aefa488e58a73b6
Pugpaprika21 <pugpaprika21@gmail.com>
2023-01-06 04:33:42 -0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_crud_2
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type NewsAggPage struct {
+	Title string
+	News  string
+}
+
+func newAggHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewsAggPage{Title: "Hahaha", News: "Some News"}
+	t, err := template.ParseFiles("template/basictemplating.html")
+
+	if err != nil {
+		panic(err)
+	}
+	t.Execute(w, p)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello GO</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)      // http://localhost:8080/
+	http.HandleFunc("/agg/", newAggHandler) // http://localhost:8080/agg/
+	http.ListenAndServe(":8080", nil)
+}
